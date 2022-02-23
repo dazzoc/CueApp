fe1038b7a70dc9a63ed093233420be84653e4e77
Christopher Dazzo <christopherdazzo@Christophers-MacBook-Air.local>
2022-02-23 10:06:41 -0600

    Config deployment

--- cueapp/settings.py
+++ cueapp/settings.py
@@ -125,3 +125,7 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
